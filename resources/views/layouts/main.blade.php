6a2f0de7a32d87b78f7078402dbc3994e8bd2211
林弘旻 <sum90707@gmail.com>
2021-11-06 17:26:25 +0800

    add https

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,15 +11,15 @@
       <script type="text/javascript" src="{{ URL::asset('js/jquery-3.3.1.min.js') }}"></script>
       <script type="text/javascript" src="{{ URL::asset('semantic/semantic.min.js') }}"></script>
       <script type="text/javascript" src="{{ URL::asset('semantic/alert/Semantic-UI-Alert.js') }}"></script>
-      <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-      <script src="//cdn.datatables.net/1.10.19/js/dataTables.semanticui.min.js"></script>
+      <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+      <script src="https://cdn.datatables.net/1.10.19/js/dataTables.semanticui.min.js"></script>
       <script type="text/javascript" src="{{ URL::asset('js/main.js') }}"></script>
 
       <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
       <link rel="stylesheet" type="text/css" href="{{ URL::asset('semantic/semantic.min.css') }}">
       <link rel="stylesheet" type="text/css" href="{{ URL::asset('semantic/alert/Semantic-UI-Alert.css') }}">
       <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/main.css') }}">
-      <link rel="stylesheet" href="//cdn.datatables.net/1.10.21/css/dataTables.semanticui.min.css">
+      <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.semanticui.min.css">
       <!-- Global site tag (gtag.js) - Google Analytics -->
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q630LEB5JW"></script>
       <script>
