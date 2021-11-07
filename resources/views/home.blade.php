3c27d6b2a1e3b6e3c0f4aba27f57cf7730aa0d74
林弘旻 <sum90707@gmail.com>
2021-11-07 22:16:49 +0800

    fix style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,31 +14,38 @@
     
 
 
-  <div class="ui one column centered grid" style="margin-top: 5%">
-    <div class="ui card">
-      <div class="image">
-        <img src="{{ URL::asset('upload/1/shot.png') }}">
-      </div>
-      <div class="content">
-        <div class="header">開發者</div>
-      </div>
-      <div class="content">
-        <div class="meta">
-          <span class="date"> Henry Lin</span><br>
-          <span class="date"> Birthday :  06/03 </span><br>
-          <span class="date"> Job :  Full-stack Developer </span><br>
+  <div class="ui two column centered grid" style="margin-top: 5%">
+
+    <div class="ten wide column">
+      @include('home.comment')
+    </div>
+
+
+    <div class="right floated middle aligned five wide column">
+      <div class="ui card">
+        <div class="image">
+          <img src="{{ URL::asset('upload/1/shot.png') }}">
         </div>
-        <div class="description">
-          <span>期望這個網站</span><br>
-          <span>能夠變成一個能夠幫助教師們</span><br>
-          <span>解決出試題困難的一個工具</span>
+        <div class="content">
+          <div class="header">開發者</div>
+        </div>
+        <div class="content">
+          <div class="meta">
+            <span class="date"> Henry Lin</span><br>
+            <span class="date"> Birthday :  06/03 </span><br>
+            <span class="date"> Job :  Full-stack Developer </span><br>
+          </div>
+          <div class="description">
+            <span>期望這個網站</span><br>
+            <span>能夠變成一個能夠幫助教師們</span><br>
+            <span>解決出試題困難的一個工具</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
     
-
-
+    
+  </div>
 
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/home.css') }}">
 
---
+++ resources/views/home/comment.blade.php
@@ -0,0 +1,43 @@
+<div class="ui comments">
+    <div class="comment">
+      <a class="avatar">
+        <img src="https://semantic-ui.com/images/avatar/small/joe.jpg">
+      </a>
+      <div class="content">
+        <a class="author">test1</a>
+        <div class="metadata">
+          <div class="date">1 天前</div>
+        </div>
+        <div class="text">
+          <p> test ui test ui test ui test ui test ui test ui test ui test ui test ui test ui </p>
+          <p>test ui test ui test ui test ui test ui test ui test ui test ui test ui test ui test ui </p>
+        </div>
+        <div class="actions">
+          <a class="reply">Reply</a>
+        </div>
+      </div>
+    </div>
+    <div class="comment">
+      <a class="avatar">
+        <img src="https://semantic-ui.com/images/avatar/small/christian.jpg">
+      </a>
+      <div class="content">
+        <a class="author">警察</a>
+        <div class="metadata">
+          <div class="date">2 天前</div>
+        </div>
+        <div class="text">test ui test ui test ui test ui test ui test ui test ui test ui  </div>
+        <div class="actions">
+          <a class="reply">Reply</a>
+        </div>
+      </div>
+    </div>
+    <form class="ui reply form">
+      <div class="field">
+        <textarea></textarea>
+      </div>
+      <div class="ui primary submit labeled icon button disabled">
+        <i class="icon edit"></i> Add Comment
+      </div>
+    </form>
+  </div>
